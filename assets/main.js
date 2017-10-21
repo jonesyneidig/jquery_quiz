eb05f5d559787a0ef8664fd7f62d33a2599605af
john_bootcamp <jonesyneidig@gmail.com>
2017-10-20 23:21:01 -0500

    intial load

---
+++ assets/main.js
@@ -0,0 +1,129 @@
+$(document).ready(function() {
+
+
+
+// Create a function that creates the start button and initial screen
+
+function initialScreen() {
+	startScreen = "<p class='text-center main-button-container'><a class='btn btn-primary btn-lg btn-block start-button' href='#' role='button'>Start Quiz</a></p>";
+	$(".mainArea").html(startScreen);
+}
+
+initialScreen();
+
+//Create a function, generateHTML(), that is triggered by the start button, and generates the HTML seen on the project video...
+
+$("body").on("click", ".start-button", function(event){
+	event.preventDefault();  // added line to test issue on GitHub Viewer
+	clickSound.play();
+	generateHTML();
+
+	timerWrapper();
+
+}); // Closes start-button click
+
+$("body").on("click", ".answer", function(event){
+	//answeredQuestion = true;
+	clickSound.play();
+	selectedAnswer = $(this).text();
+	if(selectedAnswer === correctAnswers[questionCounter]) {
+		//alert("correct");
+
+		clearInterval(theClock);
+		generateWin();
+	}
+	else {
+		//alert("wrong answer!");
+		clearInterval(theClock);
+		generateLoss();
+	}
+}); // Close .answer click
+
+$("body").on("click", ".reset-button", function(event){
+	clickSound.play();
+	resetGame();
+}); // Closes reset-button click
+
+});  //  Closes jQuery wrapper
+
+function generateLossDueToTimeOut() {
+	unansweredTally++;
+	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>You ran out of time!  The correct answer was: " + correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='img/x.png'>";
+	$(".mainArea").html(gameHTML);
+	setTimeout(wait, 4000);  //  change to 4000 or other amount
+}
+
+function generateWin() {
+	correctTally++;
+	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Correct! The answer is: " + correctAnswers[questionCounter] + "</p>";
+	$(".mainArea").html(gameHTML);
+	setTimeout(wait, 4000);  //  change to 4000 or other amount
+}
+
+function generateLoss() {
+	incorrectTally++;
+	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Wrong! The correct answer is: "+ correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='img/x.png'>";
+	$(".mainArea").html(gameHTML);
+	setTimeout(wait, 4000); //  change to 4000 or other amount
+}
+
+function generateHTML() {
+	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>30</span></p><p class='text-center'>" + questionArray[questionCounter] + "</p><p class='first-answer answer'>A. " + answerArray[questionCounter][0] + "</p><p class='answer'>B. "+answerArray[questionCounter][1]+"</p><p class='answer'>C. "+answerArray[questionCounter][2]+"</p><p class='answer'>D. "+answerArray[questionCounter][3]+"</p>";
+	$(".mainArea").html(gameHTML);
+}
+
+function wait() {
+	if (questionCounter < 7) {
+	questionCounter++;
+	generateHTML();
+	counter = 30;
+	timerWrapper();
+	}
+	else {
+		finalScreen();
+	}
+}
+
+function timerWrapper() {
+	theClock = setInterval(thirtySeconds, 1000);
+	function thirtySeconds() {
+		if (counter === 0) {
+			clearInterval(theClock);
+			generateLossDueToTimeOut();
+		}
+		if (counter > 0) {
+			counter--;
+		}
+		$(".timer").html(counter);
+	}
+}
+
+function finalScreen() {
+	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>All done, here's how you did!" + "</p>" + "<p class='summary-correct'>Correct Answers: " + correctTally + "</p>" + "<p>Wrong Answers: " + incorrectTally + "</p>" + "<p>Unanswered: " + unansweredTally + "</p>" + "<p class='text-center reset-button-container'><a class='btn btn-primary btn-lg btn-block reset-button' href='#' role='button'>Reset The Quiz!</a></p>";
+	$(".mainArea").html(gameHTML);
+}
+
+function resetGame() {
+	questionCounter = 0;
+	correctTally = 0;
+	incorrectTally = 0;
+	unansweredTally = 0;
+	counter = 30;
+	generateHTML();
+	timerWrapper();
+}
+
+var startScreen,
+	gameHTML,
+	counter = 30,
+	questionArray = ["Which 1993 film won the Best Picture Oscar at the 66th Academy Awards?", "1942 Romanitc drama starring Humphrey Bogart and Ingrid Bergman?", "What 1999 film did a boy claim to see dead people?","What actor has played Richard Kimble, Colonel Lucas, and the President of the United States?", "In what Dustin Hoffman film does he cross dress for an acting role?", "Which classic horror series features Michael Myers?", "What 1996 movie did Bill Murray play ruthless bowler Ernie McCracken?", "How much money did John Cusack's family owe the paperboy in 1985's Better Off Dead?"],
+	answerArray = [["The Piano", "Schindler's List", "In the Name of the Father", "The Fugitive"], ["Key Largo","The African Queen","Casablanca","The Big Sleep"], ["The Unforgiven", "Premonition", "The Uninvited", "The Sixth Sense"], ["Harrison Ford","Mel Gibson","Kevin Kline","Tom Hanks"], ["Rain Man", "Little Big Man", "Tootsie", "Sleepers"], ["Halloween","Friday the 13th","Nightmare on Elm Street","Psycho"], ["One more Strike", "King Pin", "Gutter Balls", "PBA"], ["$2","$150","$6","$20"]],
+	correctAnswers = ["B. Schindler's List", "C. Casablanca", "D. The Sixth Sense", "A. Harrison Ford", "C. Tootsie", "B. Friday the 13th", "B. King Pin", "A. $2"],
+	questionCounter = 0,
+	selecterAnswer,
+	theClock,
+	correctTally = 0,
+	incorrectTally = 0,
+	unansweredTally = 0,
+	clickSound = new Audio("assets/sound/button.mp3");
+
